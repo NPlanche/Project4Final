f5ae02eb2af3b34c02b1aad2158326c44f13c150
NPlanche <89564236+NPlanche@users.noreply.github.com>
2023-04-13 21:08:22 -0400

    added log in

--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,21 @@
 #Store all the views 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 #Blueprint of our application
 #all urls 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET','POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html', b=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods = ['GET','POST'])
 def signup():
     return render_template('signup.html')
 
